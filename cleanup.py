5d1083a32c833a6ac2f2c7b934da3db35b052231
Thompson <gmthomp@b0be83757e4e.ant.amazon.com>
2023-02-23 18:38:43 -0800

    files

--- cleanup.py
+++ cleanup.py
@@ -5,11 +5,6 @@ from botocore.exceptions import ClientError
 def force_delete_service(cluster, service):
     client = boto3.client('ecs')
     try:
-        response = client.update_service(
-            cluster=cluster,
-            service=service,
-            desiredCount=0
-        )
         response = client.delete_service(
             cluster=cluster,
             service=service,
@@ -24,21 +19,55 @@ def force_delete_service(cluster, service):
         else:
             print("Unexpected error: %s" % e)
 
+def check_queue():
+    sqs = boto3.client('sqs', region_name='us-east-1')
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/997477333591/my-queue'
+    response = sqs.get_queue_attributes(
+        QueueUrl=queue_url,
+        AttributeNames=['ApproximateNumberOfMessages']
+    )
+    num_messages = int(response['Attributes']['ApproximateNumberOfMessages'])
+    if num_messages > 0:
+        return False
+    else:
+        return True
+
 def lambda_handler(event, context):
     pipeline_client = boto3.client('codepipeline')
     cluster = 'ecs-ec2'
     service = 'scaling-replication-clone'
     print(event)
+    completion = False
 
     sJobId = event['CodePipeline.job']['id']
     ct = event['CodePipeline.job']['data'].get('continuationToken')
+    
+    if ct is None:
+        ct = 0
+    else:
+        ct = int(ct)
+    
+    attempts = ct + 5
 
-    completion = False
 
     while completion is False:
-        print('Not done')
+        completion = check_queue()
+        if completion is True:
+            break
+        else:
+            time.sleep(10)
+            ct += 1
+            print('Attempt', ct, 'Failed')
+            if (attempts>ct):
+                print('Maximum Attempts Tried. Retrying with Continuation Token Value:', ct)
+                response = pipeline_client.put_job_success_result(
+                    jobId=sJobId,
+                    continuationToken=str(ct))
+                exit()   
+
+
         
-    force_delete_service('my-cluster', 'my-service')
+    force_delete_service(cluster, service)
 
     response = pipeline_client.put_job_success_result(
             jobId=sJobId)
